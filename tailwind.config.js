d6bf04320c19fe13de49c33f8e998b5746a15e43
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-02-05 20:13:22 -0500

    se agrego las imagenes y svgs, ademas se actualizo el observador de tailwind, se agrego las variables de diseno en la api de tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,29 +2,44 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "./public/*.{html,js}"],
   theme: {
-    extend: {},
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'white': '#ffffff',
-      'primary': {
-        100: '#cffafe',
-        200: '#a5f3fc',
-        300: '#67e8f9',
-        400: '#22d3ee',
-        500: '#06b6d4',
-        600: '#0891b2',
-        700: '#0e7490',
-        800: '#155e75',
-        900: '#164e63',
+    extend: {
+      backgroundImage: {
+      'sanFrancisco': "url('../img/sanFrancisco.jpg')",
+			'sanFranciscoDesktop': "url('../img/sanFranciscoDesktop.jpg')",
+			'yosemite': "url('../img/yosemite.jpg')",
+			'LA': "url('../img/LA.jpg')",
+			'seattle': "url('../img/seattle.jpg')",
+			'new_york': "url('../img/new_york.jpg')",
+			'norway': "url('../img/norway.jpg')",
+			'sydney': "url('../img/sydney.jpg')",
+			'miami': "url('../img/miami.jpg')",
+			'switzerland': "url('../img/switzerland.jpg')",
+			'bali': "url('../img/bali.jpg')",
+			'norway': "url('../img/norway.jpg')",
+			'chicago': "url('../img/chicago.jpg')",
+			'europe': "url('../img/europe.jpg')",
+			'iceland': "url('../img/iceland.jpg')",
       },
-    }
+      backgroundColor: theme =>({
+        //el theme es importante ya que sino no nos dejaria usar el color
+        ...theme('colors'),
+        'primary': '#CC2D4A',
+        'secondary': '#8FA206',
+        'tertiary': '#61AEC9',
+      }),
+      textColor:{
+        'primary': '#CC2D4A',
+        'secondary': '#8FA206',
+        'tertiary': '#61AEC9',
+      },
+      fontFamily:{
+        Montserrat: ['Monserrat', 'sans-serif']
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/aspect-ratio'),
     require("@tailwindcss/forms"),
 		require("@tailwindcss/typography"),
   ],
-
 }
-
