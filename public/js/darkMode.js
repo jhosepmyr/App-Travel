3e7ecd05612f12e9ae3faa761d9247129a49304a
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-02-07 21:22:36 -0500

    se agrego las animacion para el dark mode

--- public/js/darkMode.js
+++ public/js/darkMode.js
@@ -1,26 +1,29 @@
-// On page load or when changing themes, best to add inline in `head` to avoid FOUC
-if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    document.documentElement.classList.add('dark')
-  } else {
-    document.documentElement.classList.remove('dark')
-  }
-  
-  // Whenever the user explicitly chooses light mode
-  localStorage.theme = 'light'
-  
-  // Whenever the user explicitly chooses dark mode
-  localStorage.theme = 'dark'
-  
-  // Whenever the user explicitly chooses to respect the OS preference
-  localStorage.removeItem('theme')
-
 const htmlElement = document.querySelector('html');
 const toggleButtonM = document.querySelector('#toggleDarkMode-mobile');
 const toggleButtonD = document.querySelector('#toggleDarkMode-desktop');
+const moonSun = document.querySelector('#moon-sun');
+
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.documentElement.classList.add('dark');
+    moonSun.classList.add('theme-toggle--toggled');
+    toggleButtonD.classList.add('theme-toggle--toggled');
+  } else {
+    document.documentElement.classList.remove('dark');
+    moonSun.classList.remove('theme-toggle--toggled');
+    toggleButtonD.classList.remove('theme-toggle--toggled');
+  }
+
+  localStorage.theme = 'light'
+
+  localStorage.theme = 'dark'
+
+  localStorage.removeItem('theme')
 
 const toggleDarkMode = () => {
     htmlElement.classList.toggle("dark");
-  };
+    moonSun.classList.toggle("theme-toggle--toggled");
+    toggleButtonD.classList.toggle("theme-toggle--toggled");
+};
 
 toggleButtonM.addEventListener('click', toggleDarkMode);
 toggleButtonD.addEventListener('click', toggleDarkMode);
